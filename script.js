254dbface212e32afe043ef388636aa5eb398412
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2021-11-22 12:01:30 +0000

    added new images (do not change the names) and added some comments to javascript file

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener("DOMContentLoaded", init);
-
+// adds curreent year after copyright symbol, Daniel Morrissey
 function init(){
 	let currentDate = document.getElementById("currentYear");
 	currentDate.innerHTML = "&copy;" + new Date().getFullYear();
 }
 
+// JQuery for recommendations page, user can search via genre from a drop down menu or search a band via text input (case sensitive)
 $(document).ready(function(){
 	let bands = ["Rise Against", "Green Day", "Metallica", "Taylor Swift", "Pink", "Justin Bieber", "Rihanna", "Beyonce", "Drake", "Johnny Cash", "John Denver", "Steve Earle"];
 	
@@ -20,47 +21,49 @@ $(document).ready(function(){
 		if(genre.length>0 && band.length>0){
 			$("#suggestions").text("Either select a music genre or search similar artists");
 		} else {
+			// searched based off of genre
 			if(genre.length==0){
 				$("#suggestions").text("");
 			} else{
 				if(genre == "rock"){
-					suggestionFiller(rock);
+					suggestionFiller(rock, "Rock");
 				} else if(genre == "rnb"){
-					suggestionFiller(rnb);
+					suggestionFiller(rnb, "RnB");
 				} else if(genre == "pop"){
-					suggestionFiller(pop);
+					suggestionFiller(pop, "Pop");
 				} else if(genre == "country"){
-					suggestionFiller(country);
+					suggestionFiller(country, "Country");
 				}
 			}
 			
+			// search off band name
 			if(bandSearch(band, rock)){
-				bandSearchFiller(rock, band);
-			} else if(bandSearch(band, rnb)){
+				bandSearchFiller(rock, band, "Rock");
+			} else if(bandSearch(band, rnb, "RnB")){
 				bandSearchFiller(rnb, band);
 			} else if(bandSearch(band, pop)){
-				bandSearchFiller(pop, band);
+				bandSearchFiller(pop, band, "Pop");
 			} else if(bandSearch(band, country)){
-				bandSearchFiller(country, band);
+				bandSearchFiller(country, band, "Country");
 			}
 		}
 	})
 	
-	function suggestionFiller(genre){
+	function suggestionFiller(genre, musicGenre){
 		$("#suggestions").text("");
 		var bandHolder = "";
 		for(var i = 0; i < genre.length; i++){
-			bandHolder+=genre[i] + "<br/><img src='../images/paper.jpg' style='height:300px;width:375px;'/><br/>"
+			bandHolder+=genre[i] + "<br/>Music Genre: " + musicGenre + "<br/><br/><img src='../images/" + genre[i] +".jpg' class='suggestion' style='height:300px;display:block;margin-left:auto;margin-right:auto;' alt='image of band (not actually bands to avoid copyright issues)'/><br/>"
 		}
 		$("#suggestions").html(bandHolder);
 	}
 	
-	function bandSearchFiller(genre, band){
+	function bandSearchFiller(genre, band, musicGenre){
 		$("#suggestions").text("");
 		var bandHolder="";
 		for(var i = 0; i < genre.length; i++){				
 			if(genre[i]!=band){
-				bandHolder+=genre[i] + "<br/><img src='../images/paper.jpg' style='height:300px;width:375px;'/><br/>"
+				bandHolder+=genre[i] + "<br/>Music Genre: " + musicGenre + "<br/><br/><img src='../images/" + genre[i] + ".jpg' class='suggestion' style='height:300px;display:block;margin-left:auto;margin-right:auto;' alt='image of band (not actually bands to avoid copyright issues)'/><br/>"
 			} else{
 				continue;				
 			}

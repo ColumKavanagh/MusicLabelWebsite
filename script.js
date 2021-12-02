5b4372a77c74e64d7322d4d37f39e802a600e3ff
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2021-12-02 13:35:51 +0000

    slider carousel on homepage

--- script.js
+++ script.js
@@ -149,21 +149,11 @@ $(document).ready(function(){
 	})
 });
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change every 5 seconds
-}
-
+//Homepage SLIDER code, Colum Kavanagh - WORK-IN-PROGRESS
+// var myCarousel = document.querySelector('#carouselExampleDark')
+// var carousel = new bootstrap.Carousel(myCarousel)
+// carousel.ride = "carousel";
+// carousel.interval = 500;
 
 
 
